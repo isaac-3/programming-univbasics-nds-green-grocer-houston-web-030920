aa891268ec984e01a2727a8d93e1f0d74526b2d2
isaac c <isaacgc0596@gmail.com>
2020-02-06 20:55:41 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -87,6 +87,20 @@ end
   # REMEMBER: This method **should** update cart
 
 def checkout(cart, coupons)
+  consolidated_cart = consolidate_cart(cart)
+  couponed_cart = apply_coupons(consolidated_cart)
+  final_cart = apply_clearance(couponed_cart)
+  total = 0
+  counter = 0
+  while counter < final_cart.length do
+    total += final_cart[counter][:price] * final_cart[counter][:count]
+    counter += 1
+  end
+  if total > 100
+    total -= (total * 0.10)
+  end
+  total
+end
   # Consult README for inputs and outputs
   #
   # This method should call
@@ -96,4 +110,3 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
-end
